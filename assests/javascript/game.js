c494b18348f9215f00ac491b48d4db0c97906538
Your Name <you@example.com>
2019-09-08 02:10:00 -0400

    pseudo coding complete in js

---
+++ assests/javascript/game.js
@@ -0,0 +1,13 @@
+// Andre Pseudo Code CLAP!!
+// create array for different pokemon
+// use the array of pokemon to cycle through a function
+// make a function to use the pokemon names as a set amount of letters to be pressed
+// try and make a starting image for each pokemon to be guessed
+// create a function to store variable values
+// have game move through array without restarting the page
+
+var words = ["charizard", "snorlax", "pikachu", "ponyta", "machoke", "meowth", "psyduck"]
+
+var randomPokemon = "";
+var totalLetters = [];
+var blanks = 0;
