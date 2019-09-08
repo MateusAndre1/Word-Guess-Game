1c0b49a4a195874c5719fca3e2e6e058556faaa4
Your Name <you@example.com>
2019-09-08 02:49:37 -0400

    audio files have been hadded to js and initial reset funtion created

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -2,9 +2,11 @@
 // create array for different pokemon
 // use the array of pokemon to cycle through a function
 // make a function to use the pokemon names as a set amount of letters to be pressed
+// have a picture of a guessed pokemon appear
+// also include an mp3 with the correct guess
 // try and make a starting image for each pokemon to be guessed
 // create a function to store variable values
-// have game move through array without restarting the page
+// have game restart through array without refreshing the page
 // console.log as much as possible to check functions working properly
 
 var pokemon = ["charizard", "snorlax", "pikachu", "ponyta", "machoke", "meowth", "psyduck"]
@@ -26,5 +28,49 @@ function game() {
         blanksPlusCorrect.push("_");   
     }
     document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
-    console.log(randomPokemon)
+    // console.log()
+}
+var charizard = document.getElementById("charizard");
+var snorlax = document.getElementById("snorlax");
+var pikachu = document.getElementById("pikachu");
+var ponyta = document.getElementById("ponyta");
+var machoke = document.getElementById("machoke");
+var meowth = document.getElementById("meowth");
+var psyduck = document.getElementById("psyduck");
+
+function audio() {
+    if (randomPokemon === pokemon[0]) {
+        charizard.play();
+        document.getElementById("startimg").src ="./assests/images/charizard.gif";
+    }
+    else if (randomPokemon === pokemon[1]) {
+        snorlax.play();
+        document.getElementById("startimg").src ="./assests/images/snorlax.gif";
+    }
+    else if (randomPokemon === pokemon[2]) {
+        pikachu.play();
+        document.getElementById("startimg").src ="./assests/images/pikachu.gif";
+    }
+    else if (randomPokemon === pokemon[3]) {
+        ponyta.play();
+        document.getElementById("startimg").src ="./assests/images/ponyta.gif";
+    }
+    else if (randomPokemon === pokemon[4]) {
+        machoke.play();
+        document.getElementById("startimg").src ="./assests/images/machoke.gif";
+    }
+    else if (randomPokemon === pokemon[5]) {
+        meowth.play();
+        document.getElementById("startimg").src ="./assests/images/meowth.gif";
+    }
+    else if (randomPokemon === pokemon[6]) {
+        psyduck.play();
+        document.getElementById("startimg").src ="./assests/images/psyduck.gif";
+    }
+}
+function reset () {
+    guessesRemaining = 9;
+    wrongGuess = [];
+    blanksPlusCorrect = [];
+    game ()
 }
